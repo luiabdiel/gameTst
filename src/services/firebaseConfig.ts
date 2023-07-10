74895ffc28719d4586eb2da97fe0b50731a66103
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-07-09 22:04:15 -0300

    feat: add realTime dataBase config

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,6 +1,7 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from 'firebase/auth';
+import { initializeApp, FirebaseApp, getApps } from 'firebase/app';
+import { getDatabase, Database, set, ref, update, get, child } from 'firebase/database';
+import { getAnalytics, Analytics } from "firebase/analytics";
+import { getAuth, Auth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDp4kzsKOVjNyxnyLvo8mSLw_jcXckjK9E",
@@ -9,9 +10,24 @@ const firebaseConfig = {
   storageBucket: "app-masters-games.appspot.com",
   messagingSenderId: "1078249194578",
   appId: "1:1078249194578:web:869f25c5f869874c404b8b",
-  measurementId: "G-V8MC15450V"
+  measurementId: "G-V8MC15450V",
+  databaseURL: "https://app-masters-games-default-rtdb.firebaseio.com/"
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const auth = getAuth(app);
+let app: FirebaseApp;
+let analytics: Analytics;
+let auth: Auth;
+let dataBase: Database;
+
+if (!getApps().length) {
+  try {
+    app = initializeApp(firebaseConfig);
+    analytics = getAnalytics(app);
+    auth = getAuth(app);
+    dataBase = getDatabase(app);
+  } catch (error) {
+    console.log("Não foi possível inicializar o app");
+  }
+}
+
+export { app, analytics, auth, dataBase }
