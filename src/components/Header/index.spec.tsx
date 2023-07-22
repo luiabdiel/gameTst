f164bf520fb4c84138651638e0364538f89866de
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-07-22 00:17:03 -0300

    test: fixing page title rendering test

--- src/components/Header/index.spec.tsx
+++ src/components/Header/index.spec.tsx
@@ -1,11 +1,24 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import { Header } from ".";
+import { vi } from "vitest";
+
+const mockedUseNavigate = vi.fn();
+
+vi.mock("react-router-dom", async () => {
+  const actual = await vi.importActual("react-router-dom");
+  return {
+    //@ts-ignore
+    ...actual,
+    useNavigate: () => mockedUseNavigate,
+  };
+})
 
 describe("Header", () => {
   it("Should render the site logo", () => {
-    const { getByRole } = render(<Header />);
+    const { getByText } = render(<Header />)
 
-    expect(getByRole("heading")).toBeInTheDocument();
+    const titleElement = getByText('App Masters Games');
+    expect(titleElement).toBeInTheDocument();
   });
 });
